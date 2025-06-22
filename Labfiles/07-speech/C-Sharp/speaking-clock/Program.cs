cb78f75158c9424d5e4b18c2611e89a50fbc6b5c
Devesh <devesh.v19@gmail.com>
2025-06-22 13:10:39 +0530

    Update Program.cs based on https://microsoftlearning.github.io/mslearn-ai-language/Instructions/Labs/07-speech.html
    This has current example that should have mic and speaker. When I tested this through CLI console on browser, it was not able to have access to mic and speaker. Ideally it should be reafctored into multiple methods.

--- Labfiles/07-speech/C-Sharp/speaking-clock/Program.cs
+++ Labfiles/07-speech/C-Sharp/speaking-clock/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 // Import namespaces
+using Azure.Identity;
+using Azure.AI.Projects;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 
 namespace speaking_clock
@@ -26,6 +30,8 @@ namespace speaking_clock
                 string location = configuration["LOCATION"];
 
                 // Configure speech service
+                speechConfig = SpeechConfig.FromSubscription(projectKey, location);
+                Console.WriteLine("Ready to use speech service in " + speechConfig.Region);
 
 
                 // Get spoken input
@@ -48,9 +54,51 @@ namespace speaking_clock
             string command = "";
             
             // Configure speech recognition
+            // string audioFile = "time.wav";
+            // using AudioConfig audioConfig = AudioConfig.FromWavFileInput(audioFile);
+            // using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            // Configure speech recognition
+            using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            Console.WriteLine("Speak now...");
+
+            SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+            if (speech.Reason == ResultReason.RecognizedSpeech)
+            {
+                command = speech.Text;
+                Console.WriteLine(command);
+            }
+            else
+            {
+                Console.WriteLine(speech.Reason);
+                if (speech.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(speech);
+                    Console.WriteLine(cancellation.Reason);
+                    Console.WriteLine(cancellation.ErrorDetails);
+                }
+            }
 
 
             // Process speech input
+            // Console.WriteLine("Listening...");
+            // SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+            // if (speech.Reason == ResultReason.RecognizedSpeech)
+            // {
+            //     command = speech.Text;
+            //     Console.WriteLine(command);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(speech.Reason);
+            //     if (speech.Reason == ResultReason.Canceled)
+            //     {
+            //         var cancellation = CancellationDetails.FromResult(speech);
+            //         Console.WriteLine(cancellation.Reason);
+            //         Console.WriteLine(cancellation.ErrorDetails);
+            //     }
+            // }
 
 
             // Return the command
@@ -59,13 +107,60 @@ namespace speaking_clock
 
         static async Task TellTime()
         {
+            // var now = DateTime.Now;
+            // string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+                        
+            // // Configure speech synthesis
+            // var outputFile = "output.wav";
+            // speechConfig.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+            // using var audioConfig = AudioConfig.FromWavFileOutput(outputFile);
+            // using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+
+
+            // // Synthesize spoken output
+            // // SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+            // // if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            // // {
+            // //     Console.WriteLine(speak.Reason);
+            // // }
+            // // else
+            // // {
+            // //     Console.WriteLine("Spoken output saved in " + outputFile);
+            // // }
+
+            // // Synthesize spoken output
+            // string responseSsml = $@"
+            //     <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+            //         <voice name='en-GB-LibbyNeural'>
+            //             {responseText}
+            //             <break strength='weak'/>
+            //             Time to end this lab!
+            //         </voice>
+            //     </speak>";
+            // SpeechSynthesisResult speak = await speechSynthesizer.SpeakSsmlAsync(responseSsml);
+            // if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            // {
+            //     Console.WriteLine(speak.Reason);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Spoken output saved in " + outputFile);
+            // }
+
             var now = DateTime.Now;
             string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
-                        
-            // Configure speech synthesis
 
+            // Configure speech synthesis
+            speechConfig.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+            using var audioConfig = AudioConfig.FromDefaultSpeakerOutput();
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
 
             // Synthesize spoken output
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
 
 
             // Print the response
