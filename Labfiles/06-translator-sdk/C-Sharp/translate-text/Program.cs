70020241f2a8e92bfaa3dd27289bdcca7889774c
Devesh <devesh.v19@gmail.com>
2025-06-22 11:58:02 +0530

    Update Program.cs based on https://microsoftlearning.github.io/mslearn-ai-language/Instructions/Labs/06-translate-text.html

--- Labfiles/06-translator-sdk/C-Sharp/translate-text/Program.cs
+++ Labfiles/06-translator-sdk/C-Sharp/translate-text/Program.cs
@@ -5,8 +5,9 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 
-// import namespaces
-
+ // import namespaces
+ using Azure;
+ using Azure.AI.Translation.Text;
 
 
 namespace translate_text
@@ -28,15 +29,50 @@ namespace translate_text
                 string translatorKey = configuration["TranslatorKey"];
 
 
-                // Create client using endpoint and key
+                 // Create client using endpoint and key
+                AzureKeyCredential credential = new(translatorKey);
+                TextTranslationClient client = new(credential, translatorRegion);
 
 
 
-                // Choose target language
+                 // Choose target language
+                Response<GetSupportedLanguagesResult> languagesResponse = await client.GetSupportedLanguagesAsync(scope:"translation").ConfigureAwait(false);
+                GetSupportedLanguagesResult languages = languagesResponse.Value;
+                Console.WriteLine($"{languages.Translation.Count} languages available.\n(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)");
+                Console.WriteLine("Enter a target language code for translation (for example, 'en'):");
+                string targetLanguage = "xx";
+                bool languageSupported = false;
+                while (!languageSupported)
+                {
+                    targetLanguage = Console.ReadLine();
+                    if (languages.Translation.ContainsKey(targetLanguage))
+                    {
+                        languageSupported = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{targetLanguage} is not a supported language.");
+                    }
+
+                }
 
 
 
-                // Translate text
+                 // Translate text
+                string inputText = "";
+                while (inputText.ToLower() != "quit")
+                {
+                    Console.WriteLine("Enter text to translate ('quit' to exit)");
+                    inputText = Console.ReadLine();
+                    if (inputText.ToLower() != "quit")
+                    {
+                        Response<IReadOnlyList<TranslatedTextItem>> translationResponse = await client.TranslateAsync(targetLanguage, inputText).ConfigureAwait(false);
+                        IReadOnlyList<TranslatedTextItem> translations = translationResponse.Value;
+                        TranslatedTextItem translation = translations[0];
+                        string sourceLanguage = translation?.DetectedLanguage?.Language;
+                        Console.WriteLine($"'{inputText}' translated from {sourceLanguage} to {translation?.Translations[0].TargetLanguage} as '{translation?.Translations?[0]?.Text}'.");
+                    }
+                } 
 
 
                 
